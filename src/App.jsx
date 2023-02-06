48eb86152c616a2503f4adf5b38d033e9f8ace25
saputhebeast <sapumalwijekoon4@gmail.com>
2023-02-06 10:55:03 +0530

    Basic configuration completed

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { BrowserRouter } from 'react-router-dom'
+import AnimatedRoutes from './routes'
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <AnimatedRoutes/>
+    </BrowserRouter>
+  )
+}
+
+export default App
